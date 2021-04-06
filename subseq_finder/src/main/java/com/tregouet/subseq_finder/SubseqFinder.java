b598d42c7c434d1f92f5ca22a9953b8165df89ab
gael-tregouet <gael.tregouet@gmail.com>
2021-04-06 21:33:50 +0200

    Subseq SubseqFinder ready for unit tests

--- subseq_finder/src/main/java/com/tregouet/subseq_finder/SubseqFinder.java
+++ subseq_finder/src/main/java/com/tregouet/subseq_finder/SubseqFinder.java
@@ -1,11 +1,8 @@
 package com.tregouet.subseq_finder;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import com.tregouet.subseq_finder.exceptions.SubseqException;
@@ -17,7 +14,7 @@ public class SubseqFinder {
 	private final boolean[][] dotPlot;
 	private final Set<Subseq> subseqs = new HashSet<Subseq>();
 	
-	public SubseqFinder(String[] first, String[] second) {
+	public SubseqFinder(String[] first, String[] second) throws SubseqException {
 		this.value1 = new String[first.length + 2];
 		this.value2 = new String[second.length + 2];
 		this.value1[0] = "START";
@@ -36,24 +33,41 @@ public class SubseqFinder {
 		setSubseqs();
 	}
 	
-	private void setSubseqs() {
-		Set<Subseq> subseqs = new HashSet<Subseq>();
-		Set<Subseq> nonMaximalSubseqs = new HashSet<Subseq>();
+	private void setSubseqs() throws SubseqException {
+		List<Subseq> allSubseqs = new ArrayList<Subseq>();
 		int seqMaxSize = value1.length;
-		if (value2.length > seqMaxSize)
-			seqMaxSize = value2.length;
-		if (dotPlot != null 
-				&& dotPlot[0][0] == true 
-				&& value1[0] == Subseq.SEQ_START 
-				&& value2[0] == Subseq.SEQ_START) {
+		if (value1.length > value2.length)
+			seqMaxSize = value1.length;
+		else seqMaxSize = value2.length;
+		if (dotPlot != null && dotPlot[0][0] == true) {
 			Subseq newSeq = new Subseq(seqMaxSize);
 			try {
-				subseqs.addAll(continueSubseq(newSeq, 0, 0));
-				HERE
+				allSubseqs.addAll(continueSubseq(newSeq, 0, 0));
 			}
-			
+			catch (SubseqException e) {
+				throw new SubseqException("SubseqFinder.setSubseqs() : " + System.lineSeparator() + e.getMessage());
+			}
 		}
-		
+		else throw new SubseqException("SubseqFinder.setSubseqs() : initial dotPlot values are invalid");
+		Set<Integer> nonMaxIndexes = new HashSet<Integer>();
+		for (int i = 0; i < subseqs.size() ; i++) {
+			if (!nonMaxIndexes.contains(i)) {
+				for (int j=i+1 ; j < subseqs.size() ; j++) {
+					if (!nonMaxIndexes.contains(j)) {
+						if (allSubseqs.get(i).isASubseqOf(allSubseqs.get(j))) {
+							nonMaxIndexes.add(i);
+						}
+						else if (allSubseqs.get(j).isASubseqOf(allSubseqs.get(i))) {
+							nonMaxIndexes.add(j);
+						}
+					}
+				}
+			}
+		}
+		for (int k = 0 ; k < allSubseqs.size() ; k++) {
+			if (!nonMaxIndexes.contains(k))
+				subseqs.add(allSubseqs.get(k));
+		}
 	}
 	
 	private Set<Subseq> continueSubseq(Subseq currentSubseq, int newElemVal1Coord, int newElemVal2Coord) throws SubseqException {
